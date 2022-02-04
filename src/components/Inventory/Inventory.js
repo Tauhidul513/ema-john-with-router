a32e64bcdd249ba2df23bf582e4291c0beaf9b81
Tauhidul513 <65249077+Tauhidul513@users.noreply.github.com>
2022-02-04 23:23:03 +0600

    added

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h2>Tis is inventory pages.</h2>
+        </div>
+    );
+};
+
+export default Inventory;
